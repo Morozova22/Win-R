3245ec9e6c15a2925614dfbeb409c6ecc6974e20
Seruline <106003560+Seruline@users.noreply.github.com>
2022-05-24 23:25:50 +0800

    Add files via upload

---
+++ front schedule/style.php
@@ -0,0 +1,77 @@
+<style>
+
+    .yellow{
+        font-weight: bold;
+        text-align: center;
+        color: #8804b4;
+        pointer-events: none;
+        padding: 10px;
+
+    }
+    .tbody{
+        border: 0px;
+        border-radius: 20px;
+    }
+    .yell{
+        display: none;
+    }
+    table{
+        font-size: 14px;
+        font-weight: normal;
+        width: 1050px;
+        table-layout:fixed;
+        position: absolute;
+        top:10%;
+        left: 17%;
+        height: 500px;
+        padding: 3px;
+        border-collapse: collapse;
+        border-radius: 10px;
+        font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+
+
+    }
+    td:hover{
+        background-color: rgba(200, 162, 200, 0.23);
+    }
+
+    tr:hover{
+        background-color: rgba(200, 162, 200, 0.23);
+    }
+
+    td {
+        padding: 1px 10px 1px 4px;
+        border: 2px solid #c8a2c8;
+
+    }
+    ::-webkit-scrollbar {
+        width: 15px;
+    }
+
+    ::-webkit-scrollbar-track {
+        -webkit-box-shadow: 5px 5px 5px -5px rgba(34, 60, 80, 0.2) inset;
+        background-color: #f9f9fd;
+        border-radius: 10px;
+    }
+
+    ::-webkit-scrollbar-thumb {
+        border-radius: 10px;
+        background: linear-gradient(180deg, #c8a2c8, #005bea);
+    }
+
+    body {
+        font-family: -apple-system, BlinkMacSystemFont, sans-serif;
+        background: white;
+    }
+    #container{
+        border-radius: 20px;
+        background: #ffffff;
+        box-shadow:  20px 20px 60px #cccccc,
+        -20px -20px 60px #ffffff;
+        position: absolute;
+        top:10%;
+        left: 17%;
+        width: 1050px;
+        height: 900px;
+    }
+</style>
